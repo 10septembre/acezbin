2cb0410279d10f6294027b0135917394c19bf3db
acidre <jeanluc.nguyen@gmail.com>
2012-04-24 16:44:19 +0200

    Adding the file itself

--- bin/php/acsubtreemove.php
+++ bin/php/acsubtreemove.php
@@ -1,16 +1,16 @@
 #!/usr/bin/env php
 <?php
 /**
- * File containing the mfsubtreemove.php script.
+ * File containing the acsubtreemove.php script.
  *
- * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version  2011.10
  * @package kernel
  */
 
 // Subtree Remove Script
-// file  extension/adminmf/bin/php/mfsubtreemove.php
+// file  extension/acezbin/bin/php/acsubtreemove.php
 
 // script initializing
 require 'autoload.php';
